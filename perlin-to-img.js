2607ac7a105975150f1f731baeada888928147a7
Geras <77939124+Gerassu@users.noreply.github.com>
2023-07-04 20:44:29 +0200

    Removed dependency from perlin-noise

--- perlin-to-img.js
+++ perlin-to-img.js
@@ -1,5 +1,4 @@
 // imports~
-const perlin = require('perlin-noise');
 const sharp = require('sharp');
 
 
@@ -7,26 +6,131 @@ const sharp = require('sharp');
 
     // Width and height of the image
 
-        const width = 32;
-        const height = 32;
+        const width = 512;
+        const height = 512;
 
     // Image size using sharp
 
-        const imgSize = 1024;
+        const imgSize = 512;
     
     // Perlin Options (Octave Count, Amplitude, Persistence)
 
-        const OTC = 3;                                          // 1 - 32
-        const AMP = 0.8;
-        const PER = 0.4;                                         // 0.1 - 1 
+        const OTC = 18;                                         
+        const AMP = 0.6;
+        const PER = 0.8;                                         
 
 
-// Generating a perlin noise
+// Generating perlin noise
 
-const data = perlin.generatePerlinNoise(width, height, {
+function generatePerlinNoise(width, height, options) {
+
+    options = options || {};
+
+    var octaveCount = options.octaveCount || 4;
+    var amplitude = options.amplitude || 0.1;
+    var persistence = options.persistence || 0.2;
+    var whiteNoise = generateWhiteNoise(width, height);
+  
+    var smoothNoiseList = new Array(octaveCount);
+    var i;
+
+    for (i = 0; i < octaveCount; ++i) {
+
+      smoothNoiseList[i] = generateSmoothNoise(i);
+
+    }
+
+    var perlinNoise = new Array(width * height);
+    var totalAmplitude = 0;
+
+    for (i = octaveCount - 1; i >= 0; --i) {
+
+      amplitude *= persistence;
+      totalAmplitude += amplitude;
+  
+      for (var j = 0; j < perlinNoise.length; ++j) {
+
+        perlinNoise[j] = perlinNoise[j] || 0;
+        perlinNoise[j] += smoothNoiseList[i][j] * amplitude;
+
+      }
+
+    }
+
+    for (i = 0; i < perlinNoise.length; ++i) {
+
+        perlinNoise[i] /= totalAmplitude;
+
+    }
+  
+    return perlinNoise;
+  
+
+
+function generateSmoothNoise(octave) {
+
+      var noise = new Array(width * height);
+      var samplePeriod = Math.pow(2, octave);
+      var sampleFrequency = 1 / samplePeriod;
+      var noiseIndex = 0;
+
+      for (var y = 0; y < height; ++y) {
+
+        var sampleY0 = Math.floor(y / samplePeriod) * samplePeriod;
+        var sampleY1 = (sampleY0 + samplePeriod) % height;
+        var vertBlend = (y - sampleY0) * sampleFrequency;
+
+        for (var x = 0; x < width; ++x) {
+
+          var sampleX0 = Math.floor(x / samplePeriod) * samplePeriod;
+          var sampleX1 = (sampleX0 + samplePeriod) % width;
+          var horizBlend = (x - sampleX0) * sampleFrequency;
+  
+          var top = lerp(whiteNoise[sampleY0 * width + sampleX0], whiteNoise[sampleY1 * width + sampleX0], vertBlend);
+          var bottom = lerp(whiteNoise[sampleY0 * width + sampleX1], whiteNoise[sampleY1 * width + sampleX1], vertBlend);
+
+          noise[noiseIndex] = lerp(top, bottom, horizBlend);
+          noiseIndex += 1;
+
+        }
+      }
+
+      return noise;
+
+    }
+  }
+
+
+
+function generateWhiteNoise(width, height) {
+
+    var noise = new Array(width * height);
+
+    for (var i = 0; i < noise.length; ++i) {
+
+      noise[i] = Math.random();
+
+    }
+
+    return noise;
+
+}
+
+
+
+function lerp(x0, x1, y) {
+
+    return x0 * (1 -y) + y * x1;
+
+}
+
+
+const data = generatePerlinNoise(width, height, {
+
     octaveCount: OTC,
     amplitude: AMP,
     persistence: PER,
+
 });
 
 // Creating a new image using sharp
@@ -41,9 +145,9 @@ for (let y = 0; y < imgSize; y++) {
         const value = data[Math.floor(y / (imgSize / height)) * width + Math.floor(x / (imgSize / width))];
         const color = Math.floor(value * 255);
         
-        imageBuff[index + 0] = color;
-        imageBuff[index + 1] = color;
-        imageBuff[index + 2] = color
+        imageBuff[index + 0] = color; 
+        imageBuff[index + 1] = color;   // 0 = R 1 = B 2 = G
+        imageBuff[index + 2] = color;
 
     }
 
